81721b4a2879666054950a293cf29a2192f27084
armanmac <arman.piloyan@gmail.com>
2018-12-21 02:54:15 +0400

    finished version

--- src/main/java/Layer.java
+++ src/main/java/Layer.java
@@ -1,32 +1,61 @@
-import ij.ImagePlus;
-import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
-import ij.process.ByteProcessor;
-import ij.IJ;
-import ij.plugin.filter.Convolver;
-import ij.plugin.filter.GaussianBlur;
-import java.util.Arrays;
+
+
 import java.awt.Color;
 
+@SuppressWarnings("Duplicates")
+public class Layer {
 
-public class Layer implements PlugInFilter {
-
-    public double bottom(int x) {
+    //0-1
+    public double bottom01(int x) {
         return 0.9848 * x - 6.7474;
     }
 
-    public double top(int x) {
+    public double top01(int x) {
+        return -0.0009 * x * x + 1.1917 * x - 4.0146;
+    }
+
+    //1-2
+    public double bottom12(int x) {
+        return -0.0009 * x * x + 1.1917 * x - 4.0146;
+    }
+
+    public double top12(int x) {
+        return -0.0011 * x * x + 1.2262 * x + 4.0264;
+    }
+
+    //2-3
+    public double bottom23(int x) {
+        return -0.0011 * x * x + 1.2262 * x + 4.0264;
+    }
+
+    public double top23(int x) {
+        return -0.0013 * x * x + 1.2608 * x + 12.067;
+    }
+
+    //3-4
+    public double bottom34(int x) {
+        return -0.0013 * x * x + 1.2608 * x + 12.067;
+    }
+
+    public double top34(int x) {
         return -0.0026 * x * x + 1.5713 * x + 14.8;
     }
 
-    public int setup(String args, ImagePlus im) {
-        return DOES_RGB; // this plugin accepts 8-bit grayscale images
+    //union
+    private double bottom(int x) {
+        return 0.9848 * x - 6.7474;
     }
 
-    public void run(ImageProcessor ip) {
+    private double top(int x) {
+        return -0.0026 * x * x + 1.5713 * x + 14.8;
+    }
+
+    public ImageProcessor run(ImageProcessor input) {
+        ImageProcessor ip = input.duplicate();
         int width = ip.getWidth();
         int height = ip.getHeight();
-        int pixel, r, g, b;
+        int r, g, b;
 
         double rb;
         Color color;
@@ -39,14 +68,113 @@ public class Layer implements PlugInFilter {
                 b = color.getBlue();
                 rb = (r + b) / 2.;
                 if (b < g && g < r && rb >= bottom(g) && rb <= top(g)) {
-                    // IJ.log(Integer.toString(row) + " " + Integer.toString(col));
                     ip.putPixel(col, row, 0); //BLACK
-                }
-                else
+                } else
                     ip.putPixel(col, row, 16777215); //WHITE
             }
         }
-
+        return ip;
     }
+
+    public ImageProcessor run01(ImageProcessor input) {
+        ImageProcessor ip = input.duplicate();
+        int width = ip.getWidth();
+        int height = ip.getHeight();
+        int r, g, b;
+
+        double rb;
+        Color color;
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                color = new Color(ip.getPixel(col, row));
+                r = color.getRed();
+                g = color.getGreen();
+                b = color.getBlue();
+                rb = (r + b) / 2.;
+                if (b < g && g < r && rb >= bottom01(g) && rb <= top01(g)) {
+                    ip.putPixel(col, row, 0); //BLACK
+                } else
+                    ip.putPixel(col, row, 16777215); //WHITE
+            }
+        }
+        return ip;
+    }
+
+    public ImageProcessor run12(ImageProcessor input) {
+        ImageProcessor ip = input.duplicate();
+        int width = ip.getWidth();
+        int height = ip.getHeight();
+        int r, g, b;
+
+        double rb;
+        Color color;
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                color = new Color(ip.getPixel(col, row));
+                r = color.getRed();
+                g = color.getGreen();
+                b = color.getBlue();
+                rb = (r + b) / 2.;
+                if (b < g && g < r && rb >= bottom12(g) && rb <= top12(g)) {
+                    ip.putPixel(col, row, 0); //BLACK
+                } else
+                    ip.putPixel(col, row, 16777215); //WHITE
+            }
+        }
+        return ip;
+    }
+
+    public ImageProcessor run23(ImageProcessor input) {
+        ImageProcessor ip = input.duplicate();
+        int width = ip.getWidth();
+        int height = ip.getHeight();
+        int r, g, b;
+
+        double rb;
+        Color color;
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                color = new Color(ip.getPixel(col, row));
+                r = color.getRed();
+                g = color.getGreen();
+                b = color.getBlue();
+                rb = (r + b) / 2.;
+                if (b < g && g < r && rb >= bottom23(g) && rb <= top23(g)) {
+                    ip.putPixel(col, row, 0); //BLACK
+                } else
+                    ip.putPixel(col, row, 16777215); //WHITE
+            }
+        }
+        return ip;
+    }
+
+    public ImageProcessor run34(ImageProcessor input) {
+        ImageProcessor ip = input.duplicate();
+        int width = ip.getWidth();
+        int height = ip.getHeight();
+        int r, g, b;
+
+        double rb;
+        Color color;
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                color = new Color(ip.getPixel(col, row));
+                r = color.getRed();
+                g = color.getGreen();
+                b = color.getBlue();
+                rb = (r + b) / 2.;
+                if (b < g && g < r && rb >= bottom34(g) && rb <= top34(g)) {
+                    ip.putPixel(col, row, 0); //BLACK
+                } else
+                    ip.putPixel(col, row, 16777215); //WHITE
+            }
+        }
+        return ip;
+    }
+
 
 }
--- src/main/java/LoadHistograms.java
+++ src/main/java/LoadHistograms.java
@@ -11,6 +11,10 @@ import java.util.Map;
 import java.util.stream.IntStream;
 import javax.imageio.ImageIO;
 
+/**
+@author armanpiloyan
+ */
+
 public class LoadHistograms extends Component {
 
     public static Map<String, Map<Integer, Integer>> averageHistogram = new HashMap<>();
@@ -21,6 +25,23 @@ public class LoadHistograms extends Component {
 
     public double top(int x) {
         return -0.0026 * x * x + 1.5713 * x + 14.8;
+    }
+
+    public static double calculateSD(Double numArray[]) {
+        double sum = 0.0, standardDeviation = 0.0;
+        int length = numArray.length;
+
+        for (double num : numArray) {
+            sum += num;
+        }
+
+        double mean = sum / length;
+
+        for (double num : numArray) {
+            standardDeviation += Math.pow(num - mean, 2);
+        }
+
+        return Math.sqrt(standardDeviation / length);
     }
 
     public Map<String, Map<Integer, Integer>> generateHist(BufferedImage ip) {
@@ -184,6 +205,129 @@ public class LoadHistograms extends Component {
             }
         });
 
+        Layer layer = new Layer();
 
+        IntStream.range(1, 21).forEach((i) -> {
+            try {
+
+                BufferedImage bufferedImage = ImageIO.read(new File("/Users/armanmac/Documents/imageJ/src/main/resources/equalizedSet/" + i + ".jpg"));
+                ImageProcessor image = new ColorProcessor(bufferedImage);
+
+
+                ImageProcessor res = layer.run(image);
+                ImageProcessor res01 = layer.run01(image);
+                ImageProcessor res12 = layer.run12(image);
+                ImageProcessor res23 = layer.run23(image);
+                ImageProcessor res34 = layer.run34(image);
+
+                File result = new File("/Users/armanmac/Documents/imageJ/src/main/resources/layers/" + i + ".jpg");
+                File result01 = new File("/Users/armanmac/Documents/imageJ/src/main/resources/layer01/" + i + ".jpg");
+                File result12 = new File("/Users/armanmac/Documents/imageJ/src/main/resources/layer12/" + i + ".jpg");
+                File result23 = new File("/Users/armanmac/Documents/imageJ/src/main/resources/layer23/" + i + ".jpg");
+                File result34 = new File("/Users/armanmac/Documents/imageJ/src/main/resources/layer34/" + i + ".jpg");
+
+                ImageIO.write(res.getBufferedImage(), "jpg", result);
+                ImageIO.write(res01.getBufferedImage(), "jpg", result01);
+                ImageIO.write(res12.getBufferedImage(), "jpg", result12);
+                ImageIO.write(res23.getBufferedImage(), "jpg", result23);
+                ImageIO.write(res34.getBufferedImage(), "jpg", result34);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        IntStream.range(1, 21).forEach((i) -> {
+
+            BufferedImage originalBlacked = null;
+            try {
+                originalBlacked = ImageIO.read(new File("/Users/armanmac/Documents/imageJ/src/main/resources/faceTrain/" + i + ".jpg"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ImageProcessor im = new ColorProcessor(originalBlacked);
+            ImageProcessor originBlacked = layer.run(im);
+
+
+            BufferedImage bufferedImage = null;
+            try {
+                bufferedImage = ImageIO.read(new File("/Users/armanmac/Documents/imageJ/src/main/resources/equalizedSet/" + i + ".jpg"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ImageProcessor image = new ColorProcessor(bufferedImage);
+
+            int width = image.getWidth();
+            int height = image.getHeight();
+
+
+            BufferedImage bufferedImageBlack = null;
+
+            try {
+                bufferedImageBlack = ImageIO.read(new File("/Users/armanmac/Documents/imageJ/src/main/resources/layers/" + i + ".jpg"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ImageProcessor imageBlack = new ColorProcessor(bufferedImageBlack);
+
+            int sumX = 0;
+            int sumY = 0;
+            int n = 0;
+
+
+            List<Double> xs = new ArrayList<>();
+            List<Double> ys = new ArrayList<>();
+
+            for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                    Color color = ((ColorProcessor) originBlacked).getColor(col, row);
+                    if (color.getBlue() == 0 && color.getGreen() == 0 && color.getRed() == 0) {
+                        n++;
+                        sumX += col;
+                        sumY += row;
+                        xs.add((double) col);
+                        ys.add((double) row);
+                    }
+                }
+            }
+
+            int centerX = sumX / n;
+            int centerY = sumY / n;
+
+            Double[] xsArr = xs.toArray(new Double[xs.size()]);
+            Double[] ysArr = ys.toArray(new Double[ys.size()]);
+
+            int SDX = (int) Math.round(calculateSD(xsArr));
+            int SDY = (int) Math.round(calculateSD(ysArr));
+
+            System.out.println("Center detected: (" + centerX + "," + centerY + ")" + " with S.D. x: " + SDX + " with S.D. y: " + SDY);
+
+            //standard deviation can be used in order to achieve better detection
+            //however I used simple formula for square capturing
+            //all we need to do in order to improve the result is to multiply row values by 1/SDY and by 1/SDX for column values respectfully
+            //the result is not bad but it would be better if we had ideally cropped face images,however we do not live in vacuum world, thus,
+            //I think the overall result is not bad
+            for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                    int val = (Math.abs(row - col + centerX - centerY) + Math.abs(row + col - centerX - centerY));
+                    if (val > 80 && val < 90) {
+                        image.putPixel(col, row, 16711680);
+                    }
+
+                }
+            }
+
+            File result = new File("/Users/armanmac/Documents/imageJ/src/main/resources/noseDetection/" + i + ".jpg");
+            try {
+                ImageIO.write(image.getBufferedImage(), "jpg", result);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Nose detected!");
+
+
+        });
     }
 }
